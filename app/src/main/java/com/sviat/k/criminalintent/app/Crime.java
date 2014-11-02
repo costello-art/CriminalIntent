7ed76734f6dabb92ff04fd0df82da5e22686e28b
Sviat Costigan <costello@users.noreply.github.com>
2014-11-03 00:53:52 +0200

    upgrades

--- app/src/main/java/com/sviat/k/criminalintent/app/Crime.java
+++ app/src/main/java/com/sviat/k/criminalintent/app/Crime.java
@@ -1,5 +1,6 @@
 package com.sviat.k.criminalintent.app;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,9 +9,28 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public void setTitle(String mTitle) {
