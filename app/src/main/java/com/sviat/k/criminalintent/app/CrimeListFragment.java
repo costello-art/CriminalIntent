7f466ce611dd040ed38825dad3b8d54661d4b3f2
Sviat Costigan <costello@users.noreply.github.com>
2014-11-03 21:28:35 +0200

    show crimes

--- app/src/main/java/com/sviat/k/criminalintent/app/CrimeListFragment.java
+++ app/src/main/java/com/sviat/k/criminalintent/app/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.sviat.k.criminalintent.app;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -16,5 +17,9 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crime_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
+
+        setListAdapter(adapter);
     }
 }
