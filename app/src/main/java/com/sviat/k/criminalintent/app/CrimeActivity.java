b64321e089a06ca8036d30d1944997bc8ca5a8c9
Sviat Costigan <costello@users.noreply.github.com>
2014-11-03 20:39:41 +0200

    refactoring

--- app/src/main/java/com/sviat/k/criminalintent/app/CrimeActivity.java
+++ app/src/main/java/com/sviat/k/criminalintent/app/CrimeActivity.java
@@ -9,7 +9,7 @@ public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new CrimeFragment(), "tagCrimeFragment").commit();
     }
