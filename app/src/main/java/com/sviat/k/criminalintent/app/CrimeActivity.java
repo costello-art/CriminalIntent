eec7911d4a81df001f790821d32f8056fa8a0999
Sviat Costigan <costello@users.noreply.github.com>
2014-11-03 23:21:24 +0200

    refactor

--- app/src/main/java/com/sviat/k/criminalintent/app/CrimeActivity.java
+++ app/src/main/java/com/sviat/k/criminalintent/app/CrimeActivity.java
@@ -2,14 +2,17 @@ package com.sviat.k.criminalintent.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+
+import java.util.UUID;
 
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 
     @Override
